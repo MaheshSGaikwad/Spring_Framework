0c7ed8d82b40a3e82e27738b5de1187b69417fcd
MaheshSGaikwad <maheshgaikwad193@gmail.com>
2025-01-02 10:54:28 +0530

    Bean scope Demo

--- src/main/java/com/example/spring/PhonePayPaymentProcessor.java
+++ src/main/java/com/example/spring/PhonePayPaymentProcessor.java
@@ -1,9 +1,20 @@
 package com.example.spring;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/**
+ * Bean Scope - Spring bean scope will control how many times a spring can be initiated in the container.
+ - Default scope of any spring appln is singleton. i.e bean will created at the time of startup & will
+   be reused throughout the appln context.
+  Here we need not to specify the singleton scope.
+ - Prototype scope - It will create the beans each time it requested.
+ To check that we can print the reference of that bean.
+ */
 @Component
+//@Scope("singleton")
+@Scope("prototype")
 public class PhonePayPaymentProcessor implements IPaymentProcessor{
     public void proceedPayment(Double amount){
         if (amount != 0){
--- src/main/java/com/example/spring/ShoppingCartApplication.java
+++ src/main/java/com/example/spring/ShoppingCartApplication.java
@@ -6,16 +6,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ShoppingCartApplication {
 	public static void main(String[] args) {
-		System.out.println("Setter Injection Using the xml config:");
-		ApplicationContext xmlContext = new ClassPathXmlApplicationContext("spring.xml");
-		ShoppingCart cart = xmlContext.getBean("shoppingCart", ShoppingCart.class);
-		cart.checkOut(10.5);
-		System.out.println();
+//		System.out.println("Setter Injection Using the xml config:");
+//		ApplicationContext xmlContext = new ClassPathXmlApplicationContext("spring.xml");
+//		ShoppingCart cart = xmlContext.getBean("shoppingCart", ShoppingCart.class);
+//		cart.checkOut(10.5);
+//		System.out.println();
 
 		System.out.println("Setter Injection Using the Java config:");
 		ApplicationContext javaConfigContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		ShoppingCart shoppingCart = javaConfigContext.getBean("shoppingCart", ShoppingCart.class);
 		shoppingCart.checkOut(10.5);
+
+		/**
+		 *  While using the default scope, it will print the exact same address but by using the prototype it will give the diff address for processor1 & processor2
+		 */
+		PhonePayPaymentProcessor processor1 = javaConfigContext.getBean("phonePayPaymentProcessor", PhonePayPaymentProcessor.class);
+		System.out.println(processor1);
+
+		PhonePayPaymentProcessor processor2 = javaConfigContext.getBean("phonePayPaymentProcessor", PhonePayPaymentProcessor.class);
+		System.out.println(processor2);
 	}
 
 }
