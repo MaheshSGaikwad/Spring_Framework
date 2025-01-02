9f5a0172854495cb521776d81cab8de1cb07fa9b
MaheshSGaikwad <maheshgaikwad193@gmail.com>
2025-01-02 22:04:19 +0530

    Bean lifecycle - InitializingBean & DisposableBean

--- src/main/java/com/example/spring/PhonePayPaymentProcessor.java
+++ src/main/java/com/example/spring/PhonePayPaymentProcessor.java
@@ -1,26 +1,39 @@
 package com.example.spring;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * Bean Scope - Spring bean scope will control how many times a spring can be initiated in the container.
- - Default scope of any spring appln is singleton. i.e bean will created at the time of startup & will
-   be reused throughout the appln context.
-  Here we need not to specify the singleton scope.
- - Prototype scope - It will create the beans each time it requested.
- To check that we can print the reference of that bean.
+ Bean Lifecycle- beans lifecycle involves the initialization ,bean usage & destruction.
+ We can perform it using 3 way
+ 1. InitializingBean, DisposableBean
+  - By implementing these two methods & overriding the afterPropertiesSet() & destroy() method we can achieve the bean lifecycle.
+  - in order to show the implementation of both the method we have to call the registerShutdownHook().
+ Note- prototype scope will not manage the destruction of beans,so whenever we call the registerShutdownHook() method
+    it will just give the implementation of afterProperties() but not destroy()
  */
 @Component
 //@Scope("singleton")
 @Scope("prototype")
-public class PhonePayPaymentProcessor implements IPaymentProcessor{
+public class PhonePayPaymentProcessor implements IPaymentProcessor, InitializingBean, DisposableBean {
     public void proceedPayment(Double amount){
         if (amount != 0){
             System.out.println("Payment of "+amount+"$ done successfully using Phone Pay.");
         }
         else
             throw new RuntimeException("Please enter the valid amount.");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Destroyed properties");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Setting properties after Bean is initialized");
     }
 }
--- src/main/java/com/example/spring/ShoppingCartApplication.java
+++ src/main/java/com/example/spring/ShoppingCartApplication.java
@@ -6,13 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ShoppingCartApplication {
 	public static void main(String[] args) {
-//		System.out.println("Setter Injection Using the xml config:");
-//		ApplicationContext xmlContext = new ClassPathXmlApplicationContext("spring.xml");
-//		ShoppingCart cart = xmlContext.getBean("shoppingCart", ShoppingCart.class);
-//		cart.checkOut(10.5);
-//		System.out.println();
 
-		System.out.println("Setter Injection Using the Java config:");
 		ApplicationContext javaConfigContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		ShoppingCart shoppingCart = javaConfigContext.getBean("shoppingCart", ShoppingCart.class);
 		shoppingCart.checkOut(10.5);
@@ -25,6 +19,12 @@ public class ShoppingCartApplication {
 
 		PhonePayPaymentProcessor processor2 = javaConfigContext.getBean("phonePayPaymentProcessor", PhonePayPaymentProcessor.class);
 		System.out.println(processor2);
+
+		/**(
+		 *  registerShutdownHook will gives the implementation of afterPropertiesSet() method & destroy()
+		  i.e. will print the whatever written in those method
+		 */
+		((AnnotationConfigApplicationContext) javaConfigContext).registerShutdownHook();
 	}
 
 }
